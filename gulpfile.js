aac0c75c88b234de8b9d23378bfc897b8a803787
Daniel Pino <dpino@humboldt.org.co>
2018-06-06 09:30:54 -0500

    Js and css structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,22 @@ var minifyCSS = require('gulp-csso');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
-var inlinesource = require('gulp-inline-source');
-var postcss = require('gulp-postcss');
+var sass = require('gulp-sass');
+
+
 
 
 gulp.task('html', function(){
   return gulp.src('./src/html/*.html')
-    .pipe(inlinesource({compress: true}))
-    .pipe(gulp.dest('./dist/'))
+  .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('styles', function(){
-  return gulp.src('./src/styles/**/*.css')
-    .pipe(sourcemaps.init())
-    .pipe(postcss())
+  return gulp.src('./src/styles/*.scss')
+    .pipe(sass())
     .pipe(concat('main.css'))
-    .pipe(sourcemaps.write('.'))
     .pipe(minifyCSS())
-    .pipe(gulp.dest('./dist/css'))
-
-    
+    .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('javascript', function(){
@@ -31,16 +27,18 @@ gulp.task('javascript', function(){
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest('./dist/'))
 });
 
- 
+gulp.task('images', function(){
+  return gulp.src('./src/images/*.{svg,png,gif,jpg}')
+    .pipe(gulp.dest('./dist/img'))
+});
 
- 
 gulp.task('watch', function () {
   gulp.watch('./src/javascript/*.js', ['javascript']);
   gulp.watch('./src/styles/**/*.scss', ['styles']);
   gulp.watch('./src/html/*.html', ['html']);
 });
 
-gulp.task('default', [ 'javascript', 'html', 'styles', 'watch' ]);
+gulp.task('default', [ 'javascript', 'html', 'styles', 'watch', 'images' ]);
